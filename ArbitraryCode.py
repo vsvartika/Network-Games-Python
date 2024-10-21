20c436c9e79d35527b0bcebdf55f23012ccd1d98
Vartika Singh <vsingh@uccs.edu>
2024-10-21 16:50:22 -0600

    code to solve three classes

--- ArbitraryCode.py
+++ ArbitraryCode.py
@@ -38,7 +38,7 @@ def LinearProgram(A,b,c):
   prob += lp.lpSum([c[i]*x[i] for i in set_n])
 
   for i in set_m:
-    prob += lp.lpSum([A[i][j]*x[j] for j in set_n]) >= b[i]
+    prob += lp.lpSum([A[i][j]*x[j] for j in set_n]) <= b[i]
   prob.solve()
   x_soln = np.array([x[i].varValue for i in set_n]) 
   z = prob.objective.value()
@@ -85,6 +85,175 @@ def ArbtitraryIR(C):
     return IR
 
 #-------------------------------------------------------------------------------------------------------------
+def ArbitraryComputingPoA(C,N_mat,F,w):
+  k = C.shape[0] 
+  n = np.sum(C)
+  N = np.zeros((1,k))
+  for j in range(k):
+    N[0,j] = np.sum(N_mat[j,:]*C)
+
+  f0 = F[0,0:int(N[0,0])]
+  f0 = np.append(0,f0)
+  f0 = np.append(f0,0)
+  f1 = F[1,0:int(N[0,1])]
+  f1 = np.append(0,f1)
+  f1 = np.append(f1,0)
+
+  if k==3:
+    f2 = F[2,0:int(N[0,2])]
+    f2 = np.append(0,f2)
+    f2 = np.append(f2,0)
+
+  mm = int(np.max(N))
+  f_lp = np.zeros((k,mm+2))
+
+  f_lp[0,0:(int(N[0,0])+2)] = f0
+  f_lp[1,0:(int(N[0,1])+2)] = f1
+  if k==3:
+   f_lp[2,0:(int(N[0,2])+2)] = f2
+
+  w_lp = np.append(0,w)
+
+  #the objective function
+  c = np.zeros((1,k))
+  c = np.append(c,[[1]], axis =1)
+
+  #the constraints
+  IR = ArbtitraryIR(C)
+  numRowsIR = IR.shape[0]
+
+  B= np.zeros((numRowsIR+k,1))
+  A= np.zeros((numRowsIR+k,k+1))
+
+  for j in range(k):   #to ensure lambda_j is >=0
+    A[-1-k+j+1,0] = -1
+    B[-1-k+j+1]   = 0
+  
+  for t in range(numRowsIR):
+    a = np.zeros((1,k))
+    x = np.zeros((1,k))
+    b = np.zeros((1,k))
+
+    for j in range(k):
+      idx = 3*(j+1)-3
+      a[0,j] = IR[t,idx]
+      x[0,j] = IR[t,idx+1]
+      b[0,j] = IR[t,idx+2]
+    
+    At = np.sum(np.add(a,x))
+    At = int(At)
+    Bt = np.sum(np.add(b,x))
+    Bt = int(Bt)
+
+    Atj = np.zeros((1,k))
+    for j in range(k):
+      Atj[0,j] = np.sum(N_mat[j,:]*np.add(a,x))
+
+    for j in range(k):
+      temp_idx = int(Atj[0,j])
+      A[t,j] = a[0,j]*f_lp[j,temp_idx] - b[0,j]*f_lp[j,temp_idx+1]  
+
+    B[t] = - w_lp[Bt]
+    A[t,k] = -w_lp[At] 
+  val = LinearProgram(A,B,np.transpose(c))
+
+  poa_val = val[0]
+  poa = 1/poa_val
+  return poa
+
+
+def ArbitraryOptimizingPoA(C,N_mat,w):
+  
+  w_lp = np.append(0,w)
+
+  IR = ArbtitraryIR(C)
+  numRowsIR = IR.shape[0]
+
+  k = C.shape[0] 
+
+  N = np.zeros((1,k))
+  for j in range(k):
+    N[0,j] = np.sum(N_mat[j,:]*C)
+  
+  Lj = np.zeros((1,k))
+  for j in range(k):
+    Lj[0,j] = N[0,j] + 2
+
+  sum_Lj = int(np.sum(Lj))
+
+  #the objective function
+  c = np.zeros((1,sum_Lj))
+  c = np.append(c,[[1]], axis =1)
+
+  #the constraints
+  B= np.zeros((numRowsIR+4*k,1))
+  A= np.zeros((numRowsIR+4*k,sum_Lj+1))
+
+  #first 4*k constraints
+  for j in range(k):
+    r_idx = int(4*j)
+    c_idx1 =0
+    if j>=1:
+      c_idx1 = int(np.sum(Lj[0,0:j]))
+    c_idx2 = int(np.sum(Lj[0,0:j+1])-1)
+
+    A[r_idx,c_idx1]     =  1
+    A[r_idx+1,c_idx1]   = -1
+    A[r_idx+2,c_idx2]   =  1
+    A[r_idx+3,c_idx2]   = -1
+
+  B[0:4*k] = 0
+
+  #LP constraints
+  for t in range(numRowsIR):
+    a = np.zeros((1,k))
+    x = np.zeros((1,k))
+    b = np.zeros((1,k))
+
+    for j in range(k):
+      idx = 3*(j+1)-3
+      a[0,j] = IR[t,idx]
+      x[0,j] = IR[t,idx+1]
+      b[0,j] = IR[t,idx+2]
+    
+    At = np.sum(np.add(a,x))
+    At = int(At)
+    Bt = np.sum(np.add(b,x))
+    Bt = int(Bt)
+
+    Atj = np.zeros((1,k))
+    for j in range(k):
+      Atj[0,j] = np.sum(N_mat[j,:]*np.add(a,x))
+
+    for j in range(k):
+      c_idx = int(Atj[0,j])
+      if j>=1:
+        c_idx = int(np.sum(Lj[0,0:j]) + Atj[0,j])
+
+      A[t+4*k,c_idx ] = a[0,j] 
+      A[t+4*k,c_idx + 1] = -b[0,j]
+
+    B[t+4*k] = - w_lp[Bt]
+    A[t+4*k,-1] = -w_lp[At] 
+  poa_val,f = LinearProgram(A,B,np.transpose(c))
+
+  opt_poa = 1/poa_val
+  xval = f.shape
+
+  #the optimal mechanism
+  fstar = np.zeros((k,int(np.sum(C))))
+  fstar[:,:] = np.nan
+  for j in range(k):
+    init_idx = 1
+    if j>=1:
+      init_idx = int(np.sum(Lj[0,0:j])) + 1
+    fin_idx = int(np.sum(Lj[0,0:j+1])-1) 
+    # print(init_idx,fin_idx)
+    fstar[j,0:int(N[0,j])] = f[init_idx:fin_idx]
+       
+
+  return opt_poa,fstar
+
 
 
 
